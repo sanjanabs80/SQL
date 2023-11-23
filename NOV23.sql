97091c90f07d21ae7576c59602b36c7df44da396
bssanjana80 <bssanjana80@gmail.com>
2023-11-23 09:32:56 +0530

    Nov 23 class

---
+++ NOV23.sql
@@ -0,0 +1,23 @@
+/*Syntax for creating database
+CREATE DATABASE database;*/
+
+CREATE DATABASE sep_27;
+/*Syntax for Using database
+USE databasename;*/
+
+USE sep_27;
+USE sys;
+
+
+SHOW databases;
+
+
+
+
+CREATE DATABASE sep_11;
+USE sep_11;
+
+
+SHOW databases;
+
+
